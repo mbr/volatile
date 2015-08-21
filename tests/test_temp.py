eb96b7825c44e3c154c4c5ba371cdeba74d35514
Marc Brinkmann <git@marcbrinkmann.de>
2015-08-22 01:19:22 +0200

    Added tests.

--- tests/test_temp.py
+++ tests/test_temp.py
@@ -1,1 +1,32 @@
+import os
 
+from temp import NamedTemporaryFile
+import pytest
+
+
+def test_ntf_simple_persistance():
+    with NamedTemporaryFile() as tmp:
+        assert os.path.exists(tmp.name)
+        tmp.close()
+        assert os.path.exists(tmp.name)
+
+    assert not os.path.exists(tmp.name)
+
+
+def test_keeps_content():
+    with NamedTemporaryFile() as tmp:
+        tmp.write(b'foo')
+        tmp.close()
+
+        assert b'foo' == open(tmp.name, 'rb').read()
+
+
+def test_unlink_raises():
+    with pytest.raises(OSError):
+        with NamedTemporaryFile() as tmp:
+            os.unlink(tmp.name)
+
+
+def test_can_ignore_missing():
+    with NamedTemporaryFile(ignore_missing=True) as tmp:
+        os.unlink(tmp.name)
