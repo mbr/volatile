9ed454b7a2e875ef949ce13b1bea6314f843e22c
Marc Brinkmann <git@marcbrinkmann.de>
2016-04-04 12:19:57 +0200

    YaPF setup.py, bump major version (breaking changes inc.).

--- setup.py
+++ setup.py
@@ -10,17 +10,16 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-setup(
-    name='volatile',
-    version='1.3.dev1',
-    description='A small extension for the tempfile module.',
-    long_description=read('README.rst'),
-    author='Marc Brinkmann',
-    author_email='git@marcbrinkmann.de',
-    url='https://github.com/mbr/volatile',
-    license='MIT',
-    packages=find_packages(exclude=['tests']),
-    classifiers=[
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-    ])
+setup(name='volatile',
+      version='2.0.0dev1',
+      description='A small extension for the tempfile module.',
+      long_description=read('README.rst'),
+      author='Marc Brinkmann',
+      author_email='git@marcbrinkmann.de',
+      url='https://github.com/mbr/volatile',
+      license='MIT',
+      packages=find_packages(exclude=['tests']),
+      classifiers=[
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ])
